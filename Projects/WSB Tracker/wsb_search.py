233c2c9d5b2478f9dc7088d510115cce42130db4
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2021-02-26 17:13:45 -0500

    broke down search to search for $ symbols

--- Projects/WSB Tracker/wsb_search.py
+++ Projects/WSB Tracker/wsb_search.py
@@ -8,15 +8,15 @@ print('hello')
 
 start_epoch=int(dt.datetime(2017, 1, 1).timestamp())
 
-submissions = list(api.search_submissions(after=start_epoch,
-                            subreddit='wallstreetbets',
-                            filter=['url','author', 'title', 'subreddit'],
-                            limit=10))
+submissions = api.search_submissions(after=start_epoch,
+                                    subreddit='wallstreetbets',
+                                    filter=['url','author', 'title', 'subreddit'],
+                                    limit=30)
 
 for submission in submissions:
-    print(submission.created_utc)
-    print(submission.title)
-    print(submission.url)
+    #print(submission.created_utc)
+    #print(submission.title)
+    #print(submission.url)
     
     words = submission.title.split()
     cashtags = list(set(filter(lambda word: word.lower().startswith('$'), words)))
